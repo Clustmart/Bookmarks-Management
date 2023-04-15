efbfc507a4db28c1a9a57c1a40b01c80552ae628
Digital Travel Guide <contact@romaniatravel.guide>
2023-04-15 23:52:06 +0300

    Fixed some errors (if brockenLinks.txt file didn't exist), added website title and saved output fields as csv.

--- Bookmarks Management/main.swift
+++ Bookmarks Management/main.swift
@@ -8,31 +8,35 @@
 import Foundation
 
 func checkBookmarkRecursively(_ bookmark: [String: Any], parentTitle: String?, parentURL: String?, brokenLinksFile: FileHandle?) {
-    if let title = bookmark["Title"] as? String, let type = bookmark["WebBookmarkType"] as? String {
-        let url = bookmark["URLString"] as? String ?? ""
-        if type == "WebBookmarkTypeList" {
-            if let url = URL(string: url) {
-                let semaphore = DispatchSemaphore(value: 0)
-                let task = URLSession.shared.dataTask(with: url) { data, response, error in
-                    if let error = error {
-                        print("\(title) : \(url) : \(error.localizedDescription)")
-                        brokenLinksFile?.seekToEndOfFile()
-                        brokenLinksFile?.write("\(title) : \(url) : \(error.localizedDescription)\n".data(using: .utf8)!)
-                    } else if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode != 200 {
-                        print("\(title) : \(url) : Broken Link")
-                        brokenLinksFile?.seekToEndOfFile()
-                        brokenLinksFile?.write("\(title) : \(url) : Broken Link\n".data(using: .utf8)!)
-                    }
-                    semaphore.signal()
+    // let uriDict = bookmark["URIDictionary"] as? [String: Any]
+    // let title = uriDict?["title"] as! [String : Any]
+    let uriDict = bookmark["URIDictionary"] as? [String: Any]
+    let title = uriDict?["title"] as? String
+    let type = bookmark["WebBookmarkType"] as? String
+    let url = bookmark["URLString"] as? String ?? ""
+        
+    if type == "WebBookmarkTypeLeaf" {
+        if let url = URL(string: url) {
+            let semaphore = DispatchSemaphore(value: 0)
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                if let error = error {
+                    print("\(uriDict?["title"] as? String ?? "") ; \(url) ; \(error.localizedDescription)")
+                    brokenLinksFile?.seekToEndOfFile()
+                    brokenLinksFile?.write("\(uriDict?["title"] as? String ?? "") ; \(url) ; \(error.localizedDescription)\n".data(using: .utf8)!)
+                } else if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode != 200 {
+                    print("\(uriDict?["title"] as? String ?? "") ; \(url) ; Broken Link")
+                    brokenLinksFile?.seekToEndOfFile()
+                    brokenLinksFile?.write("\(uriDict?["title"] as? String ?? "") ; \(url) ; Broken Link\n".data(using: .utf8)!)
                 }
-                task.resume()
-                semaphore.wait()
+                semaphore.signal()
             }
-        } else if type == "WebBookmarkTypeLeaf" {
-            let children = bookmark["Children"] as? [[String: Any]] ?? []
-            for child in children {
-                checkBookmarkRecursively(child, parentTitle: title, parentURL: url, brokenLinksFile: brokenLinksFile)
-            }
+            task.resume()
+            semaphore.wait()
+        }
+    } else if type == "WebBookmarkTypeList" {
+        let children = bookmark["Children"] as? [[String: Any]] ?? []
+        for child in children {
+            checkBookmarkRecursively(child, parentTitle: title, parentURL: url, brokenLinksFile: brokenLinksFile)
         }
     }
 }
@@ -44,15 +48,22 @@ func checkBookmarks() {
 
     if fileManager.fileExists(atPath: fileURL.path) {
         do {
-            print(fileURL.path)
+            print("Bookmark file path:", fileURL.path)
             let data = try Data(contentsOf: fileURL)
             let plist = try PropertyListSerialization.propertyList(from: data, options: [], format: nil)
             guard let bookmarks = plist as? [String: Any], let children = bookmarks["Children"] as? [[String: Any]] else {
                 print("Error: Failed to parse bookmarks plist")
                 return
             }
+            
             // Create a file to write broken links
-            let brokenLinksFileURL = URL(fileURLWithPath: "./brokenLinks.txt")
+            let brokenLinksFilePath = "./brokenLinks.txt"
+            if !fileManager.fileExists(atPath: brokenLinksFilePath) {
+                fileManager.createFile(atPath: brokenLinksFilePath, contents: nil, attributes: nil)
+            }
+            let brokenLinksFileURL = URL(fileURLWithPath: brokenLinksFilePath)
+            let brokenLinksFile = try? FileHandle(forWritingTo: brokenLinksFileURL)
+            
             if let brokenLinksFile = FileHandle(forWritingAtPath: brokenLinksFileURL.path) {
                 for child in children {
                     checkBookmarkRecursively(child, parentTitle: nil, parentURL: nil, brokenLinksFile: brokenLinksFile)
